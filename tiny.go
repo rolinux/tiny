c30ebeb5940dbb7699f391a6612e738f50f9ba5d
Radu Coroi <radu@mbpr.local>
2019-02-09 18:51:47 +0000

    adding also with formating

--- tiny.go
+++ tiny.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-        "io"
-        "log"
-        "flag"
-        "net/http"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"runtime"
 )
 
 // parse -port as string as we need a string for ListenAndServe and WriteString
 var portPtr = flag.String("port", "8000", "port number to listen to")
 
 func port(w http.ResponseWriter, r *http.Request) {
-          io.WriteString(w, *portPtr + "\n")
+	io.WriteString(w, *portPtr+"\n")
 }
 
 func main() {
-        flag.Parse()
-        http.HandleFunc("/", port)
-        // it will fail if the command-line flag is not a port number
-        // not the most efficient string concat but speed is not the target
-        if err := http.ListenAndServe(":" + *portPtr, nil);err != nil {
-                log.Fatal("ListenAndServe: ", err)
-        }
+	runtime.GOMAXPROCS(1)
+	flag.Parse()
+	http.HandleFunc("/", port)
+	// it will fail if the command-line flag is not a port number
+	// not the most efficient string concat but speed is not the target
+	if err := http.ListenAndServe(":"+*portPtr, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
