6fcd60231842f05400861796371b28e99b052915
Radu Coroi <rolinux@users.noreply.github.com>
2019-10-16 00:06:30 +0100

    Update tiny.go

--- tiny.go
+++ tiny.go
@@ -5,20 +5,29 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 )
 
 // parse -port as string as we need a string for ListenAndServe and WriteString
-var portPtr = flag.String("port", "8000", "port number to listen to")
+// no validation of port string (maybe later)
+var portPtr = flag.String("port", "8080", "port number to listen to")
 
-func port(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, *portPtr+"\n")
+func tiny(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "localhost"
+	}
+	output := "<html><body><center>Welcome to <b>rolinux/tiny</b> app running<br>"
+	output = output + "on <b>" + hostname + ":" + *portPtr + "</b> (hostname:port)<br>on <b>"
+	output = output + runtime.GOOS + ":" + runtime.GOARCH + "</b> architecture</center></body></html>"
+	io.WriteString(w, output)
 }
 
 func main() {
 	runtime.GOMAXPROCS(1)
 	flag.Parse()
-	http.HandleFunc("/", port)
+	http.HandleFunc("/", tiny)
 	// it will fail if the command-line flag is not a port number
 	// not the most efficient string concat but speed is not the target
 	if err := http.ListenAndServe(":"+*portPtr, nil); err != nil {
